d4d8ad5ee5f2a28756d3fe7346c97fcb69f636ec
Esteban7712 <bleed.7712@gmail.com>
2023-05-27 18:52:11 -0500

    deploy

--- Back/src/DB_connection.js
+++ Back/src/DB_connection.js
@@ -4,11 +4,15 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const Character = require("./models/Character")
 const Favorite = require("./models/Favorite")
 
-
 const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+   `postgresql://postgres:aeSbLHNwYJOqTbKIMDDa@containers-us-west-188.railway.app:7039/railway`,
    { logging: false, native: false }
 );
+
+/* const sequelize = new Sequelize(
+   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/rickandmorty`,
+   { logging: false, native: false }
+); */
 
 //Model Implementation
 Character(sequelize);

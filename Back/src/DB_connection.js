6929b66c0e357c8da95446e957f2071d5ff9089d
Esteban7712 <bleed.7712@gmail.com>
2023-05-27 20:26:50 -0500

    deploy

--- Back/src/DB_connection.js
+++ Back/src/DB_connection.js
@@ -4,6 +4,11 @@ const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 const Character = require("./models/Character")
 const Favorite = require("./models/Favorite")
 
+/* const sequelize = new Sequelize(
+   "postgres://default:VPTDSaeH5ht8@ep-plain-wind-017527.us-east-1.postgres.vercel-storage.com:5432/verceldb",
+   { logging: false, native: false }
+); */
+
 const sequelize = new Sequelize(
    `postgresql://postgres:aeSbLHNwYJOqTbKIMDDa@containers-us-west-188.railway.app:7039/railway`,
    { logging: false, native: false }
